49b51ca4cf4a1ec81bedab3a57803152ef5d8f76
GianSz <giansz2003@gmail.com>
2021-10-31 14:05:16 -0500

    31/10/2021 2.05pm

--- Taller 2/Ejercicio_7.py
+++ Taller 2/Ejercicio_7.py
@@ -1,6 +1,8 @@
 # Si dijeran que la lista está necesariamente ordenada entonces se implementaría el siguiente código. La 
-# sitaución de peor caso es en la que todos lo elementos de la lista son engativos, teniendo una complejidad de 
-# O(n).
+# sitaución de peor caso es en la que todos lo elementos de la lista son negativos, teniendo una complejidad de 
+# O(n). 
+
+# El tiempo de ejecución es : 0.0007936954498291016
 def returnNegativeValuesOrdered(list):
     finalList = []
     for i in range(0, len(list)):
@@ -12,7 +14,9 @@ def returnNegativeValuesOrdered(list):
 
 # Por otro lado si dijeran que la lista no está necesariamente ordenada entonces se implementaría el siguiente
 # código. La situación de peor caso es la misma que la del caso mejor o caso regular, ya que siempre se 
-# recorre el arreglo del principio al final, teniendo como complejidad O(n).#
+# recorre el arreglo del principio al final, teniendo como complejidad O(n). 
+
+# El tiempo de ejecución es: 0.0009899139404296875
 def returnNegativeValuesDisordered(list):
     finalList = []
     for i in range(0, len(list)):
